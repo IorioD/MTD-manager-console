c292071b08625c66dc304eb5c72c6c2b0d3b0396
Daniele Iorio <81297468+IorioD@users.noreply.github.com>
2025-09-17 16:07:31 +0200

    Update NodeDTO.java

--- src/main/java/mtd/manager/dto/NodeDTO.java
+++ src/main/java/mtd/manager/dto/NodeDTO.java
@@ -15,4 +15,44 @@ public class NodeDTO implements Serializable {
     private String type;
     private Boolean available;
 
+
+    public String gethostname() {
+        return hostname;
+    }
+
+    public void sethostname (String hostname) {
+        this.hostname = hostname;
+    }
+
+    public String getipAddress() {
+        return ipAddress;
+    }
+    
+    public void setipAddress (String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole (String role) {
+        this.role = role;
+    }
+
+    public String getType() {  
+        return type;
+    }
+    
+    public void setType (String type) {
+        this.type = type;
+    }
+    
+    public Boolean getAvailable() {
+        return available;
+    }                  
+
+    public void setAvailable (Boolean available) {
+        this.available = available;
+    }
 }
