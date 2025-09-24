c58f756dd38ae79cfa1cdac7e4ffdad4b7641d33
Daniele Iorio <81297468+IorioD@users.noreply.github.com>
2025-09-24 17:28:11 +0200

    Update DeploymentDTO.java

--- src/main/java/mtd/manager/dto/DeploymentDTO.java
+++ src/main/java/mtd/manager/dto/DeploymentDTO.java
@@ -9,14 +9,63 @@ import java.io.Serializable;
 public class DeploymentDTO implements Serializable {
     private static final long serialVersionUID = 1L;
     private String name;
-
     private String namespace;
-
     private Long id;
-
     private String type;
-
+    private String nodeName;
+    private String status;
+    private String podIp;
     private Integer strategy;
-	
 	private boolean enabled;
+
+
+    public String getName() {
+        return name;
+    }  
+
+    public void setName (String name) {
+        this.name = name;
+    }  
+
+    public String getNamespace() {
+        return namespace;
+    }  
+
+    public void setNamespace (String namespace) {
+        this.namespace = namespace;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType (String type) {
+        this.type = type;
+    }
+
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    public void setNodeName (String nodeName) {
+        this.nodeName = nodeName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus (String status) {
+        this.status = status;
+    }
+
+    public String getPodIp() {
+        return podIp;
+    }
+
+    public void setPodIp(String podIp) {
+        this.podIp = podIp;
+    }
+
 }
