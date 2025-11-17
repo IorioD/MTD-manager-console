22a88ec21613231b32e99b4eaa0d615cc385acd9
Daniele Iorio <81297468+IorioD@users.noreply.github.com>
2025-11-17 20:36:56 +0100

    Update DeploymentDTO.java

--- src/main/java/mtd/manager/dto/DeploymentDTO.java
+++ src/main/java/mtd/manager/dto/DeploymentDTO.java
@@ -13,6 +13,8 @@ public class DeploymentDTO implements Serializable {
     private String namespace;
     private String status;
     private String type;
+    private Integer strategy;
+    private Boolean enabled;
 
     public String getName() {
         return name;
@@ -36,5 +38,29 @@ public class DeploymentDTO implements Serializable {
 
     public void setStatus (String status) {
         this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType (String type) {
+        this.type = type;
+    }
+
+    public Integer getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy (Integer strategy) {
+        this.strategy = strategy;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled (Boolean enabled) {
+        this.enabled = enabled;
     }
 }
