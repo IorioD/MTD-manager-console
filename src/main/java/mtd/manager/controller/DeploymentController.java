11a4b6caf2d571023387c9a9ff8581d5b96595e4
Daniele Iorio <81297468+IorioD@users.noreply.github.com>
2025-11-17 20:36:11 +0100

    Update DeploymentController.java

--- src/main/java/mtd/manager/controller/DeploymentController.java
+++ src/main/java/mtd/manager/controller/DeploymentController.java
@@ -2,6 +2,8 @@ package mtd.manager.controller;
 
 import mtd.manager.dto.DeploymentDTO;
 import mtd.manager.service.DeploymentService;
+import mtd.manager.vo.DeploymentVO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,11 @@ public class DeploymentController {
     @Autowired
     private DeploymentService deploymentService;
 
+    @PostMapping
+    public String save(@Valid @RequestBody DeploymentVO vO) {
+        return deploymentService.save(vO).toString();
+    }
+
     @GetMapping("/{id}")
     public DeploymentDTO getById(@Valid @NotNull @PathVariable("id") Long id) {
         return deploymentService.getById(id);
@@ -28,7 +35,7 @@ public class DeploymentController {
         return deploymentService.findAll();
     }
 
-    /* @PutMapping("/{id}/strategy")
+    @PutMapping("/{id}/strategy")
     public void updateDeploymentStrategy(@PathVariable("id") Long id, @RequestParam Integer strategy) {
         deploymentService.updateDeploymentStrategy(id, strategy);
     }
@@ -41,5 +48,5 @@ public class DeploymentController {
     @PatchMapping("/{id}/toggle")
     public void toggleEnabled(@PathVariable("id") Long id) {
         deploymentService.toggleEnabled(id);
-    } */
+    }
 }
