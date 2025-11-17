0fc9b800a296c722caa0193be9061fcceb4503cd
Daniele Iorio <81297468+IorioD@users.noreply.github.com>
2025-11-17 20:39:03 +0100

    Update DeploymentRepository.java

--- src/main/java/mtd/manager/repository/DeploymentRepository.java
+++ src/main/java/mtd/manager/repository/DeploymentRepository.java
@@ -2,10 +2,13 @@ package mtd.manager.repository;
 
 
 import mtd.manager.entity.Deployment;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface DeploymentRepository extends JpaRepository<Deployment, Long>, JpaSpecificationExecutor<Deployment> {
 
-    Deployment findByName(String name);
+    Optional<Deployment> findByNameAndNamespace(String name, String namespace);
 }
